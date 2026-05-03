9a2d67f0a5d3e7271cb3ef98b0af45be208ab96e
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-03 13:59:35 +0700

    Add: binary-search - submission-7

---
+++ Data Structures & Algorithms/binary-search/submission-7.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    binary_search(left, right, nums, target){
+        if(left > right) return -1;
+        let mid = left + Math.floor((right - left)/2);
+        if(nums[mid] > target){
+           return this.binary_search(left, mid - 1, nums, target);
+        }else if (nums[mid] < target){
+            return this.binary_search(mid + 1, right, nums, target);
+        }else if (nums[mid] == target){
+            return mid;
+        }
+    }
+    search(nums, target) {
+      return this.binary_search(0, nums.length - 1, nums, target);
+    }
+
+}
