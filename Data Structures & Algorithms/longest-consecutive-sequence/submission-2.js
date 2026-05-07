913655b37764d23b1caeb1ab0615a8138b9c9fb7
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-07 12:39:55 +0700

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    longestConsecutive(nums) {
+       let resNumber = 0;
+       const store = new Set(nums);
+
+       for(let i = 0 ; i <= nums.length - 1 ; i++){
+            if(!store.has(nums[i]) - 1){
+                let curr = nums[i];
+                let longgest = 0;
+
+                while(store.has(curr)){
+                    longgest++
+                    curr++
+                }
+
+                resNumber = Math.max(resNumber, longgest);
+            }       
+           }
+
+       return resNumber;
+    }
+}
