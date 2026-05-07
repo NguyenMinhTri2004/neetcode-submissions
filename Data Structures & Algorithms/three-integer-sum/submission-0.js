ca8461e5a72b25cef29af22f1ef5131e58aa9b56
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-07 15:09:57 +0700

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+       nums.sort((a,b) => a - b);
+       const count = new Map();
+
+       //dem so lan xuat hien
+       for(let num of nums){
+          count.set(num, (count.get(num) || 0) + 1)
+       }
+
+       let res = [];
+
+       for(let i = 0 ; i <= nums.length - 1; i++){
+         count.set(nums[i], count.get(nums[i]) - 1)
+         if(i > 0 && nums[i] === nums[i - 1]){
+            continue;
+         }
+
+         for(let j = i + 1; j <= nums.length - 1; j++){
+            count.set(nums[j], count.get(nums[j]) - 1)
+
+            if(j > i + 1 && nums[j] === nums[j - 1] ){
+              continue;
+            }
+
+            const target = -(nums[i] + nums[j]);
+
+            if(count.get(target) > 0){
+                res.push([nums[i], nums[j], target]);
+            }
+         }
+
+         for(let j = i + 1; j <= nums.length - 1; j++){
+              count.set(nums[j], count.get(nums[j]) + 1)
+         }
+       }
+
+       return res;
+    }
+}
