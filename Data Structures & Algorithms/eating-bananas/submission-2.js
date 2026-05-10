2f0501ce7e871df5c57de905ef3439c2b71b9a04
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-10 12:06:54 +0700

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+       let capacity = 1;
+       while(true){
+          let totalTime = 0
+          for(let i = 0; i < piles.length ; i++){
+             totalTime = totalTime + Math.ceil(piles[i]/capacity)
+          }
+
+          if(totalTime <= h){
+            return capacity;
+          }
+
+          capacity++
+       }
+    }
+}
