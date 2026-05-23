c3b3b64af6300497b35a58ce856a47eee168662e
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-23 13:25:14 +0700

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+       if(intervals.length === 1){
+        return intervals
+       }
+       let res = [];
+       intervals.sort((a,b) => a[0] - b[0]);
+       let startLast = intervals[0][0];
+       let endLast = intervals[0][1];
+
+
+       for(let i = 1 ; i < intervals.length ; i++){
+         if(intervals[i][0] <= endLast){
+            endLast = Math.max(endLast, intervals[i][1])
+         }else{
+                res.push([startLast, endLast]); // ✅ push interval cũ
+                startLast = intervals[i][0];    // ✅ chuyển pointer
+                 endLast = intervals[i][1];
+         }
+       }
+
+          res.push([startLast, endLast]); // ✅ push interval cuối cùng
+  return res;
+     }
+
+}
