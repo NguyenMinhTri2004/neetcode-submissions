3137072dbc22583a5eb7cba41d1490ee1340b432
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-26 10:08:00 +0700

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} gas
+     * @param {number[]} cost
+     * @return {number}
+     */
+    canCompleteCircuit(gas, cost) {
+        let n = gas.length;
+        for(let i = 0 ; i < n; i++) {
+            let tank = gas[i] - cost[i]
+            if(tank < 0) continue
+
+            let j = (i + 1) % n;
+
+            while(j !== i){
+                tank = tank + gas[j] - cost[j];
+                if(tank < 0) break
+                j = (j + 1) % n;
+            }
+
+            if(i === j) return i
+        }
+
+
+        return -1
+    }
+}
