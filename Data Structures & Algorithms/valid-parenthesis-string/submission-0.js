b7b207aa7bcaa26f0d25b45c84b1786a0d227bdd
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-27 09:16:50 +0700

    Add: valid-parenthesis-string - submission-0

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-0.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    checkValidString(s) {
+        let star = [];
+        let left = [];
+
+        for(let i = 0 ; i < s.length; i++){
+            const ch = s[i];
+            if(ch === '(') {
+                left.push(i)
+            }else if (ch === "*"){
+                star.push(i)
+            }else{
+                if(left.length === 0 && star.length === 0){
+                    return false
+                }
+
+
+                if(left.length > 0){
+                    left.pop()
+                }else{
+                    star.pop()
+                }
+            }
+        }
+
+
+        while(left.length > 0 && star.length > 0){
+            if(left.pop() > star.pop()) return false
+        }
+
+        return left.length === 0
+    }
+}
