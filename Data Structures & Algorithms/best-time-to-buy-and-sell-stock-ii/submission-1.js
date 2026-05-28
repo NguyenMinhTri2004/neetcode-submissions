ee06fb5c953ef0d2f5e58030a95ede87d0b66cbc
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-05-28 22:22:05 +0700

    Add: best-time-to-buy-and-sell-stock-ii - submission-1

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+      let res = 0;
+      for(let i = 1; i < prices.length; i++){
+        if(prices[i] > prices[i - 1]){
+            res = res + prices[i] - prices[i - 1]
+        }
+      }
+
+      return res
+    }
+}
