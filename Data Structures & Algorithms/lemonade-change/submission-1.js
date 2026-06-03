59bec0fae1d2f8eb16c41c57f1c2fe1435e421b4
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-06-03 09:39:30 +0700

    Add: lemonade-change - submission-1

---
+++ Data Structures & Algorithms/lemonade-change/submission-1.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} bills
+     * @return {boolean}
+     */
+    lemonadeChange(bills) {
+        let five = 0;
+        let ten = 0
+
+        for(let bill of bills){
+            if(bill === 5){
+                five++
+            }else if(bill == 10){
+                ten++
+                if(five > 0){
+                    five--
+                }else{
+                    return false
+                }
+
+            }else{  
+                if(five > 0 && ten > 0){
+                    five--
+                    ten--
+                }else if(five >= 3){
+                    five = five - 3
+                }else{
+                    return false
+                }
+            }   
+        }
+
+        return true
+    }
+}
