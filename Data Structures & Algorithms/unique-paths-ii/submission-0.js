964df033301653b0e856e3e1ad56f2872dfcaa70
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-06-14 22:22:52 +0700

    Add: unique-paths-ii - submission-0

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-0.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     * @return {number}
+     */
+    uniquePathsWithObstacles(grid) {
+        const M = grid.length,
+            N = grid[0].length;
+        const dp = Array.from({ length: M }, () => Array(N).fill(-1));
+
+        const dfs = (r, c) => {
+            if (r === M || c === N || grid[r][c] === 1) {
+                return 0;
+            }
+            if (r === M - 1 && c === N - 1) {
+                return 1;
+            }
+            if (dp[r][c] !== -1) {
+                return dp[r][c];
+            }
+            dp[r][c] = dfs(r + 1, c) + dfs(r, c + 1);
+            return dp[r][c];
+        };
+
+        return dfs(0, 0);
+    }
+}
