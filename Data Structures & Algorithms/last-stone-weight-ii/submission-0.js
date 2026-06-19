cc548223b37f07ee0733513b265ebfb46b0a04ea
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-06-19 23:40:43 +0700

    Add: last-stone-weight-ii - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight-ii/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} stones
+     * @return {number}
+     */
+    lastStoneWeightII(stones) {
+        const stoneSum = stones.reduce((a, b) => a + b, 0);
+        const target = Math.ceil(stoneSum / 2);
+        const dp = Array.from({ length: stones.length }, () =>
+            Array(target + 1).fill(-1),
+        );
+
+        const dfs = (i, total) => {
+            if (total >= target || i === stones.length) {
+                return Math.abs(total - (stoneSum - total));
+            }
+            if (dp[i][total] !== -1) {
+                return dp[i][total];
+            }
+
+            dp[i][total] = Math.min(
+                dfs(i + 1, total),
+                dfs(i + 1, total + stones[i]),
+            );
+            return dp[i][total];
+        };
+
+        return dfs(0, 0);
+    }
+}
