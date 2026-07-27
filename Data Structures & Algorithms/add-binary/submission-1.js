cef3b6322bd130f4fa7da3c96e41095fb18494d8
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-07-27 16:33:24 +0700

    Add: add-binary - submission-1

---
+++ Data Structures & Algorithms/add-binary/submission-1.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {string} a
+     * @param {string} b
+     * @return {string}
+     */
+    addBinary(a, b) {
+        let i = a.length - 1;
+    let j = b.length - 1;
+    let carry = 0;
+    let result = "";
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        let sum = carry;
+
+        if (i >= 0) {
+            sum += a[i] - '0';   // ép ký tự thành số
+            i--;
+        }
+        if (j >= 0) {
+            sum += b[j] - '0';
+            j--;
+        }
+
+        result = (sum % 2) + result;  // chữ số hiện tại
+        carry = Math.floor(sum / 2);  // nhớ sang bit tiếp theo
+    }
+
+    return result;
+    }
+}
