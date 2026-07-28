57516dd75b24ea6e99f7c3f8868f82a7042d7864
Nguyen Minh Tri <97088671+NguyenMinhTri2004@users.noreply.github.com>
2026-07-28 15:38:03 +0700

    Add: binary-search - submission-12

---
+++ Data Structures & Algorithms/binary-search/submission-12.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        return this.binarySearch(0, nums.length - 1, nums, target)
+    }
+
+    binarySearch(l, r, nums, target){
+       if(l > r){
+          return -1
+       }
+
+       let mid = l + Math.floor((r-l)/2)
+
+       if(nums[mid] == target) return mid;
+
+       return nums[mid] > target ? this.binarySearch(l, mid - 1, nums, target):
+       this.binarySearch(mid + 1, r, nums, target) 
+    }
+
+}
